9ae16d702f16a654061483e462289005ce5c3bb8
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-28 21:53:23 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
-import './styles.css';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import ReactImageGallery from 'react-image-gallery';
 
 function App() {
+
+  const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
   return (
+
+    
 
     <>
 
       <h1>Welcome to the Image Gallery</h1>
+      <ReactImageGallery items={images} />;
 
     </>
   );
